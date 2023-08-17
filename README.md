1d5c6ef91f8bb65fc4cf2cd29878eb0b51554cdd
Alexander Ordoñez <alexderdevs@gmail.com>
2023-08-17 12:10:56 -0500

    first-commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# cinemapedia
+
+# Dev
+
+1. Copy .env.template and renamed it to .env
+2. Change env variables
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:cinemapedia/config/theme/app_theme.dart';
+import 'package:cinemapedia/config/router/app_router.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: AppTheme().getTheme(),
+    );
+  }
+}
